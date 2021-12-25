660696c2b1a41a97b9da9c0bf2798224f1d722c7
Mertcan Nerse <mertcannerse@outlook.com>
2021-12-25 13:59:43 +0000

    Create ToDoHeader.js

---
+++ odev2/src/components/ToDoHeader.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function ToDoHeader({ setTodoData, todoData }) {
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+
+    let newItem = e.target[0].value;
+
+    if (newItem == "") {
+      return false;
+    }
+
+    e.target.children[0].value = "";
+
+    setTodoData([...todoData, { value: newItem }]);
+  };
+
+  return (
+    <header className="header">
+      <h1>todos</h1>
+      <form onSubmit={onSubmitForm}>
+        <input
+          name="todoEnter"
+          type="text"
+          className="new-todo"
+          placeholder="What needs to be done?"
+        ></input>
+      </form>
+    </header>
+  );
+}
+
+export default ToDoHeader;
