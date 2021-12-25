813568123abdb4c8dc0c7b4b29abefd9c4e59822
Mertcan Nerse <mertcannerse@outlook.com>
2021-12-25 14:00:06 +0000

    Create ToDoFooter.js

---
+++ odev2/src/components/ToDoFooter.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function ToDoFooter({ todoData, setTodoData }) {
+  const clearItems = () => {
+    setTodoData([]);
+  };
+  return (
+    <footer className="footer">
+      <span className="todo-count">
+        <strong> {todoData.length} </strong>
+        items left
+      </span>
+      <button className="clear-completed" onClick={clearItems}>
+        Clear completed
+      </button>
+    </footer>
+  );
+}
+
+export default ToDoFooter;
